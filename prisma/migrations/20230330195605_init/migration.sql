1eb6cc8cf2f310d29b52adbe88470dccec7e93d6
Iyiolaosuagwu <iyiolaosuagwu1@gmail.com>
2023-03-30 20:56:51 +0100

    fix: regenerated migration

--- prisma/migrations/20230330195605_init/migration.sql
+++ prisma/migrations/20230330195605_init/migration.sql
@@ -2,8 +2,8 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "username" TEXT,
-    "password" TEXT,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
